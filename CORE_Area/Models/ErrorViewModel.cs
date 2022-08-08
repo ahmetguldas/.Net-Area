cdc133a3c980dfe2eb480866c466144045572673
ahmet.guldas <ahmet.guldas@hotmail.com>
2022-08-08 22:30:54 +0300

    Add project files.

---
+++ CORE_Area/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CORE_Area.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
